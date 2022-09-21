a7e9fd72fd0d016a7d4c6fb914a29c08e6e6dc99
solitary-cloud <1946306647@qq.com>
2022-09-21 13:10:20 +0800

    🎈 perf(container): container组件细化

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -1,37 +1,12 @@
 <template>
   <el-container>
+    <el-aside width="auto">
+      <NavSide :collapse="collapse"></NavSide>
+    </el-aside>
     <el-container>
-      <el-aside width="auto">
-        <el-menu
-          :collapse="collapse"
-          active-text-color="#ffd04b"
-          background-color="#545c64"
-          class="el-menu-vertical-demo"
-          default-active="2"
-          text-color="#fff"
-          @open="handleOpen"
-          @close="handleClose"
-        >
-          <el-menu-item index="1">
-            <el-icon-menu></el-icon-menu>
-            <span>导航一</span>
-          </el-menu-item>
-          <el-menu-item index="2">
-            <el-icon-menu></el-icon-menu>
-            <span>导航一</span>
-          </el-menu-item>
-          <el-menu-item index="3">
-            <el-icon-menu></el-icon-menu>
-            <span>导航一</span>
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
       <el-main>
         <el-header>
-          <span @click="toggle">
-            <el-icon-expand v-if="collapse"></el-icon-expand>
-            <el-icon-fold v-else></el-icon-fold>
-          </span>
+          <NavHeader v-model:collapse="collapse"></NavHeader>
         </el-header>
         <router-view></router-view>
       </el-main>
@@ -41,15 +16,11 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-const collapse = ref<Boolean>(false);
+import NavHeader from "./nav-header/index.vue";
+import NavSide from "./nav-side/index.vue";
 
-const toggle = () => {
-  collapse.value = !collapse.value;
-};
+const collapse = ref<Boolean>(false);
 </script>
 
 <style scoped lang="scss">
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-}
 </style>
---
+++ src/components/container/src/nav-header/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <span @click="toggle">
+    <el-icon-expand v-if="collapse"></el-icon-expand>
+    <el-icon-fold v-else></el-icon-fold>
+  </span>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  collapse: boolean;
+}>();
+
+const emits = defineEmits(["update:collapse"]);
+
+const toggle = () => {
+  emits("update:collapse", !props.collapse);
+};
+</script>
+
+<style scoped>
+</style>
