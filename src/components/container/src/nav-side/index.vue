aacc89cbf7086e0798c3c633ea6b8265826cf1f8
solitary-cloud <1946306647@qq.com>
2022-10-03 11:25:52 +0800

    📃 docs(container): pep-menu代替原来的el-menu

--- src/components/container/src/nav-side/index.vue
+++ src/components/container/src/nav-side/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-menu
+  <!-- <el-menu
     :collapse="collapse"
     active-text-color="#ffd04b"
     background-color="#545c64"
@@ -19,13 +19,83 @@
       <el-icon-menu></el-icon-menu>
       <span>趋势标记</span>
     </el-menu-item>
-  </el-menu>
+  </el-menu> -->
+
+  <pep-menu
+    :collapse="collapse"
+    active-text-color="#ffd04b"
+    background-color="#545c64"
+    class="el-menu-vertical-demo"
+    default-active="$route.path"
+    text-color="#fff"
+    :data="data"
+  ></pep-menu>
 </template>
 
 <script setup lang="ts">
+import { MenuItem } from "../../../menu/src/types";
+
 const props = defineProps<{
   collapse: boolean;
 }>();
+
+const data: MenuItem[] = [
+  {
+    name: "首页",
+    index: "/",
+    icon: "HomeFilled",
+  },
+  {
+    name: "图标选择器",
+    index: "/icon",
+    icon: "Check",
+  },
+  {
+    name: "趋势标记",
+    index: "trend",
+    icon: "Sort",
+  },
+  {
+    name: "时间选择器",
+    index: "/time",
+    icon: "Timer",
+  },
+  {
+    name: "导航菜单",
+    index: "/menu",
+    icon: "SwitchFilled",
+  },
+  {
+    name: "省市区选择",
+    index: "/area",
+    icon: "Location",
+  },
+  {
+    name: "城市选择",
+    index: "/city",
+    icon: "School",
+  },
+  {
+    name: "进度条",
+    index: "/progress",
+    icon: "DArrowRight",
+  },
+  {
+    name: "日历",
+    index: "/cacendar",
+    icon: "Discount",
+  },
+  {
+    name: "表单",
+    index: "/form",
+    icon: "ScaleToOriginal",
+  },
+  {
+    name: "表格",
+    index: "/table",
+    icon: "Grid",
+  },
+];
 </script>
 
 <style scoped lang="scss">
--- src/components/menu/src/index.vue
+++ src/components/menu/src/index.vue
@@ -1,42 +1,44 @@
 <template>
   <el-menu :default-active="defaultActive" :router="router" v-bind="$attrs">
-    <template v-for="(menu, index) in data" :key="index">
-      <!-- 没有二级 -->
-      <el-menu-item
-        v-if="!menu.children || !menu.children.length"
-        :index="menu.index"
-      >
-        <component
-          v-if="menu.icon"
-          :is="`el-icon-${toLine(menu.icon)}`"
-        ></component>
-        <span>{{ menu.name }}</span>
-      </el-menu-item>
-
-      <!-- 存在二级 -->
-      <el-sub-menu
-        :index="menu.index"
-        v-if="menu.children && menu.children.length"
-      >
-        <template #title>
+    <el-scrollbar>
+      <template v-for="(menu, index) in data" :key="index">
+        <!-- 没有二级 -->
+        <el-menu-item
+          v-if="!menu.children || !menu.children.length"
+          :index="menu.index"
+        >
           <component
             v-if="menu.icon"
             :is="`el-icon-${toLine(menu.icon)}`"
           ></component>
           <span>{{ menu.name }}</span>
-        </template>
-        <el-menu-item
-          v-for="(child, childIndex) in menu.children"
-          :key="childIndex"
-        >
-          <component
-            v-if="child.icon"
-            :is="`el-icon-${toLine(child.icon)}`"
-          ></component>
-          <span>{{ child.name }}</span>
         </el-menu-item>
-      </el-sub-menu>
-    </template>
+
+        <!-- 存在二级 -->
+        <el-sub-menu
+          :index="menu.index"
+          v-if="menu.children && menu.children.length"
+        >
+          <template #title>
+            <component
+              v-if="menu.icon"
+              :is="`el-icon-${toLine(menu.icon)}`"
+            ></component>
+            <span>{{ menu.name }}</span>
+          </template>
+          <el-menu-item
+            v-for="(child, childIndex) in menu.children"
+            :key="childIndex"
+          >
+            <component
+              v-if="child.icon"
+              :is="`el-icon-${toLine(child.icon)}`"
+            ></component>
+            <span>{{ child.name }}</span>
+          </el-menu-item>
+        </el-sub-menu>
+      </template>
+    </el-scrollbar>
   </el-menu>
 </template>
 
@@ -47,8 +49,8 @@ import { toLine } from "../../../utils";
 const props = withDefaults(
   defineProps<{
     data: MenuItem;
-    defaultActive: string; 
-    router: boolean; 
+    defaultActive: string;
+    router: boolean;
   }>(),
   {
     defaultActive: "",
